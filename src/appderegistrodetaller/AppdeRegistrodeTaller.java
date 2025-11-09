d82e5becafaec27485e86ee8c4b75981d03210be
Jhon Fonseca <jhonkennethfonseca99@gmail.com>
2025-11-09 11:53:26 -0600

    Carga inicial

---
+++ src/appderegistrodetaller/AppdeRegistrodeTaller.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appderegistrodetaller;
+
+/**
+ *
+ * @author Jhon fonseca
+ */
+public class AppdeRegistrodeTaller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+        new MDIApplication().setVisible(true);
+    }
+    
+}
---
+++ src/inventario/Inventarios.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventario;
+
+/**
+ *
+ * @author Jhon fonseca
+ */
+public class Inventarios {
+    String mecanico;   // Nombre del mecanico tipo cadena
+    String Tipo;       // Tipo de 
+    String Modelo;
+    Double Placa;
+    int Existencia;
+}
